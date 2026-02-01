3be3e6aaedba5edb542a265cbdd3752176b1d34b
Scott "art <artisdoingstuff@gmail.com>
2026-02-01 18:42:06 +0800

    Add project files.

---
+++ No-Name/Misc/Includes.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <optional>
+#include <queue>
+#include <random>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <typeindex>
+#include <utility>
+#include <vector>
+#include <Windows.h>
+
+#include "json.hpp"
+
+using json = nlohmann::json;
